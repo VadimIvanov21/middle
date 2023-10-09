969c923b1b7098e620d9a68476161e11edeafcb3
VadimIvanov21 <144548392+VadimIvanov21@users.noreply.github.com>
2023-10-09 11:11:36 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -8,4 +8,6 @@ void itc_num_print(int number);
 int itc_len_num(long long number);
 int itc_sum_num(long long number);
 long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+int itc_min_num(long long number);
 #endif // MIDLH_H_INCLUDED
